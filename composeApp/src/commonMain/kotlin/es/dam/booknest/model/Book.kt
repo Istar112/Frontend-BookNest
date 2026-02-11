b5be29ea0a97c61d31d07ab0f45414141f8a8c76
vicferbal <victorferrandez94@gmail.com>
2026-02-11 16:58:29 +0100

    Añadido clases Book y User

--- composeApp/src/commonMain/kotlin/es/dam/booknest/model/Book.kt
+++ composeApp/src/commonMain/kotlin/es/dam/booknest/model/Book.kt
@@ -1,9 +1,9 @@
 package es.dam.booknest.model
 
 data class Book(
-     private val id: String,
+    private val id: String,
     private val isbn: String,
-    private val title:String,
+    private val title: String,
     private val category: String,
     private val totalPages: Int,
     private val publicationDate: String,
--- composeApp/src/commonMain/kotlin/es/dam/booknest/model/User.kt
+++ composeApp/src/commonMain/kotlin/es/dam/booknest/model/User.kt
@@ -1,5 +1,9 @@
 package es.dam.booknest.model
 
-class User(
-
+data class User(
+    val id: String,
+    val username: String,
+    val name: String,
+    val email: String,
+    val phone: String
 )
